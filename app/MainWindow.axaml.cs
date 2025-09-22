8097dfb1c6d6768346f426790e210a071d7f21cd
權少 <ljqican@163.com>
2025-09-22 21:07:43 +0800

    佈局示例。

--- app/MainWindow.axaml.cs
+++ app/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace app;
 
@@ -7,5 +8,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        textBox.Text = "click";
     }
 }
